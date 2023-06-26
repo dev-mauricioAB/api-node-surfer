25ed18a94e8ce05a40c51c33083d77f14f67655a
MauricioAB <dev.mauricioalezandre@gmail.com>
2023-06-26 19:39:05 -0300

    🧪 test: adding jest initial configs

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
 const root = resolve(__dirname);
+
 module.exports = {
   rootDir: root,
   displayName: 'root-tests',
@@ -10,5 +11,5 @@ module.exports = {
   moduleNameMapper: {
     '@src/(.*)': '<rootDir>/src/$1',
     '@test/(.*)': '<rootDir>/test/$1',
-  }
+  },
 };
---
+++ test/forecast.test.ts
@@ -0,0 +1,12 @@
+import supertest from "supertest";
+
+describe('Beach forecast functional tests', () => {
+  it('should return a forecast with just a few times', async () => {
+    const { body, status } = await supertest(app).get('/forecast');
+
+    expect(status).toBe(200);
+    expect(body).toBe([
+      {}
+    ])
+  })
+})
