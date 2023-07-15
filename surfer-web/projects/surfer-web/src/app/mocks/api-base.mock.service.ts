736e0f71bf500847e981c43089e9cb6a404ad368
MauricioAB <dev.mauricioalezandre@gmail.com>
2023-07-15 19:12:51 -0300

    🐞 fix: fixed eslint configs

--- surfer-web/projects/surfer-web/src/app/mocks/api-base.mock.service.ts
+++ surfer-web/projects/surfer-web/src/app/mocks/api-base.mock.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { delay, Observable, of } from 'rxjs';
+import { GetForecastsResponseModel } from '../models/api-responses.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiBaseMockService {
-  private mockConfigurations: any[] = [];
+  private mockConfigurations: GetForecastsResponseModel[] = [];
 
-  getForecasts(): Observable<any[]> {
+  getForecasts(): Observable<GetForecastsResponseModel[]> {
     return of(this.mockConfigurations)
       .pipe(delay(1000));
   }
